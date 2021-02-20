977e0a8b7c143bef4b711f5c76edf75972073dc1
vprezn <79380777+vprezn@users.noreply.github.com>
2021-02-20 23:59:06 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, abort, jsonify
+import pickle
+app=Flask(__name__)
+
+@app.route("/student_performance_prediction")
+def home():
+    model = pickle.load(open('model.pkl', 'rb'))
+    data = request.get_json()
+    return jsonify({
+                'success': True,
+                'data': model.predit([
+                            data["gender"],
+                            data["Nationalty"],
+                            data["place_of_birth"],
+                            data["stage"],
+                            data["grade"],
+                            data["section"],
+                            data["topic"],
+                            data["semester"],
+                            data["relation"],
+                            data["raisedhands"],
+                            data["visted_resource"],
+                            data["AnnouncementsView"],
+                            data["Discussion"],
+                            data["ParentAnsweringSurvey"],
+                            data["ParentschoolSatisfaction"],
+                            data["StudentAbsenceDays"],
+                        ]),
+            })
+
+
+
+
+if __name__ == "__main__":
+    app.run()(debug=False,host='0.0.0.0')
